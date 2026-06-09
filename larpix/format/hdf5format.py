48bc19f20f8719a355562522c068c43ba5e43ae3
Roberto Mandujano <robx97@gmail.com>
2026-06-09 16:14:55 -0700

    Safety in v3 packet format for non-data packets with no float_charge, v3

--- larpix/format/hdf5format.py
+++ larpix/format/hdf5format.py
@@ -899,7 +899,7 @@ def _format_packets_packet_v3_0(pkt, version='3.0', dset='packets', *args, **kwa
     encoded_packet = [0]*len(dtypes[version][dset])
     i = 0
     for value_name, value_type in dtypes[version][dset]:
-        if value_name == 'dataword' and getattr(pkt, 'packet_type', None) == 1:
+        if value_name == 'dataword' and getattr(pkt, 'float_charge', None) is not None:
             encoded_packet[i] = pkt.float_charge
         else:
             encoded_packet[i] = getattr(pkt, value_name, None)
